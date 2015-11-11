39bf8fb2e14b1ec276c3ae432c8ac1f26f4f7af7
Ted H <zymurgy.bc@gmail.com>
2015-11-10 23:43:32 -0800

    removed postfix from the initial installs -- it isn't an actual package, anyway

--- raspberrypi_setup.sh
+++ raspberrypi_setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install curl postfix mailutils
+sudo apt-get install curl mailutils ssmtp
 git config --global user.email "zymurgy.bc@gmail.com"
 git config --global user.name "Ted H."
 
