c289caa087217d8acfb522338077434402a18412
Ted H <zymurgy.bc@gmail.com>
2015-11-28 11:46:26 -0800

    various small updates

--- raspberrypi_setup.sh
+++ raspberrypi_setup.sh
@@ -1,4 +1,7 @@
-sudo apt-get install curl mailutils ssmtp
+sudo apt-get install curl mailutils ssmtp mosquitto-clients
+
+sudo cp /etc_config/etc_mail.rc /etc/mail.rc
+touch ~/dead.letter
 git config --global user.email "zymurgy.bc@gmail.com"
 git config --global user.name "Ted H."
 
