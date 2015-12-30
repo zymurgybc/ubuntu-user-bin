0471ef0ba86e4b4cf8cf8089dbf159e555cbe891
Ted H <zymurgy.bc@gmail.com>
2015-12-30 12:39:05 -0800

    Working on better off-network detection and recovery for razberry

--- check_network.sh
+++ check_network.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 # http://www.linuxquestions.org/questions/linux-networking-3/script-to-check-connection-and-restart-network-if-down-262281/
 ROUTER_IP=192.168.1.1
+TEST_LOG=/var/log/check_network.log
+
+echo ====================                >> $TEST_LOG
+echo `date +"%Y-%m-%d %T"`               >> $TEST_LOG
+echo ping result = `ping -c1 $ROUTER_IP` >> $TEST_LOG
+echo --------------------                >> $TEST_LOG
+lsusb                                    >> $TEST_LOG
+
 ( ! ping -c1 $ROUTER_IP >/dev/null 2>&1 ) && service network restart >/dev/null 2>&1
+
+if [ -z "`ping -c1 $ROUTER_IP >/dev/null 2>&1`" ]; then
+    logger Network Restart Type 2
+    service network restart >/dev/null 2>&1
+fi
