82b7ef2e0db08a9c8cb04d1524653049ee6fd388
Ted H <zymurgy.bc@gmail.com>
2016-10-04 22:34:24 -0700

    updating some of the matrix creator scripts to fix missing items

--- matrix-creator/00-Do_All.sh
+++ matrix-creator/00-Do_All.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # http://jasperproject.github.io/documentation/installation/
 
-LOGFILE=$HOME/bin/matrix-creator/00-Do_All.log
+_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+LOGFILE=$_dir/00-Do_All.log
 
 # sudo nano /etc/modprobe.d/alsa-base.conf
 # - Change the following line:
@@ -21,10 +22,46 @@ LOGFILE=$HOME/bin/matrix-creator/00-Do_All.log
 # PATH=$PATH:/usr/local/lib/
 # export PATH
 
-
+cd $_dir
 source 10-Add-Package-Repos
+cd $_dir
 source 10-NodeRed-UpgradeInstallation
+cd $_dir
+source 20-Clone_GitHub-Google
+cd $_dir
 source 20-Clone_GitHub-MatrixIO
-source 20-GetJasperProject
 
+cd $_dir
+source 30-Face-analytics
+cd $_dir
+source 30-Matrix-app-config-helper
+cd $_dir
+source 30-Matrix-cli
+cd $_dir
+source 30-Matrix-cv
+cd $_dir
+source 30-protocol-buffers
+cd $_dir
+source 30-Matrix-continuity
+cd $_dir
+source 30-Matrix-creator-alexa-voice-demo
+cd $_dir
+source 30-Matrix-creator-alexa-voice-services
+cd $_dir
+source 30-Matrix-creator-documentation
+cd $_dir
+source 30-Matrix-creator-fpga
+cd $_dir
+source 30-Matrix-creator-hal
+cd $_dir
+source 30-Matrix-creator-init
+cd $_dir
+source 30-Matrix-creator-malos
+cd $_dir
+source 30-Matrix-OS
+
+cd $_dir
+source 50-GetJasperProject
+
+#cd $_dir
 #source matrix_alexa.sh
--- raspberrypi_setup.sh
+++ raspberrypi_setup.sh
@@ -8,7 +8,7 @@ git config --global push.default simple
 git pull
 
 sudo apt-get install curl mailutils cmake ssmtp mosquitto-clients jq python-dev python-pip python3-pip
-sudo apt-get install nfs-kernel-server nfs-common portmap dos2unix
+sudo apt-get install nfs-kernel-server nfs-common portmap dos2unix libssl-dev
 sudo update-rc.d rpcbind enable
 sudo service rpcbind start
 
@@ -61,5 +61,6 @@ if [ -f "/home/pi/bin/crontab.`hostname`.bak" ]; then
     sudo crontab /home/pi/bin/crontab.`hostname`.bak
 fi
 
+perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
 sudo -H pip install --upgrade ephem pytz pika python-dateutil 
 
