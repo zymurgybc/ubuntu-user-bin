387d01300341821c773e2cc760aa77fee868ef26
Ted H <zymurgy.bc@gmail.com>
2016-11-27 09:40:25 -0800

    Adding new Matrix Creator fetch-build-repo scripts

--- matrix-creator/00-Do_All.sh
+++ matrix-creator/00-Do_All.sh
@@ -24,43 +24,81 @@ LOGFILE=$_dir/00-Do_All.log
 
 cd $_dir
 source 10-Add-Package-Repos
+
 cd $_dir
 source 10-NodeRed-UpgradeInstallation
+
 cd $_dir
 source 20-Clone_GitHub-Google
+
 cd $_dir
 source 20-Clone_GitHub-MatrixIO
 
 cd $_dir
 source 30-Face-analytics
+
 cd $_dir
 source 30-Matrix-app-config-helper
+
 cd $_dir
 source 30-Matrix-cli
+
+cd $_dir
+source matrix-io.github.io
+
+cd $_dir
+source 30-Matrix-creator-case
+
+cd $_dir
+source 30-Matrix-app-clickgames
+
 cd $_dir
 source 30-Matrix-cv
+
+cd $_dir
+source 30-Matrix-srcjs-apps
+
+cd $_dir
+source 30-Matrix-creator-malos-android
+
 cd $_dir
 source 30-protocol-buffers
+
 cd $_dir
 source 30-Matrix-continuity
+
 cd $_dir
 source 30-Matrix-creator-alexa-voice-demo
+
 cd $_dir
 source 30-Matrix-creator-alexa-voice-services
+
 cd $_dir
 source 30-Matrix-creator-documentation
+
+cd $_dir
+source 30-Matrix-docs-templates
+
 cd $_dir
 source 30-Matrix-creator-fpga
+
 cd $_dir
 source 30-Matrix-creator-hal
+
 cd $_dir
 source 30-Matrix-creator-init
+
 cd $_dir
 source 30-Matrix-creator-malos
+
 cd $_dir
 source 30-Matrix-creator-uv-demo
+
 cd $_dir
 source 30-Matrix-OS
+
+cd $_dir
+source 30-Matrix-TV-Remote
 
 cd $_dir
 source 50-GetJasperProject
