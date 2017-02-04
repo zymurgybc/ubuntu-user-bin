937a4a75f551c9ef9bf4a6d51e025b0435bd5e2b
Ted H <zymurgy.bc@gmail.com>
2017-02-03 21:41:43 -0800

    added autoremove to upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,4 @@
-sudo apt-get update \
-&& sudo apt-get -y upgrade \
-&& sudo apt-get -y dist-upgrade
-
+   sudo apt-get update          \
+&& sudo apt-get -y upgrade      \
+&& sudo apt-get -y dist-upgrade \
+&& sudo apt-get -y autoremove
