e9c1c75f8cb9f814a32e7d1e80efacdc3ad0a8f3
Ted H <zymurgy.bc@gmail.com>
2017-04-13 10:46:06 -0700

    purging stupid packages regularly since they keep coming back!

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,5 @@
    sudo apt-get update          \
 && sudo apt-get -y upgrade      \
 && sudo apt-get -y dist-upgrade \
+&& sudo apt-get -y purge libreoffice wolfram-engine sonic-pi scratch \
 && sudo apt-get -y autoremove
