b5a16559a60f6ba605c3aaee5e2375034dce516d
zymurgybc <zymurgy.bc@gmail.com>
2017-04-18 18:37:28 -0700

    suppressing errors when packages are purged but not installed

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
    sudo apt-get update          \
 && sudo apt-get -y upgrade      \
 && sudo apt-get -y dist-upgrade \
-&& sudo apt-get -y purge libreoffice wolfram-engine sonic-pi scratch \
+&& sudo apt-get -y purge libreoffice wolfram-* sonic-pi scratch 2>/dev/null \
 && sudo apt-get -y autoremove
