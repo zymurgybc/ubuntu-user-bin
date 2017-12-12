e42c8b44f175471ffcaf3ccee1a5545b6d0a71c7
Ted H <zymurgy.bc@gmail.com>
2017-12-12 08:40:58 -0800

    adding a couple python modules to a basic install

--- raspberrypi_setup.sh
+++ raspberrypi_setup.sh
@@ -68,10 +68,11 @@ if [ -f "${HOME}/bin/crontab.`hostname`.bak" ]; then
 fi
 
 sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
-sudo -H pip install --upgrade ephem pytz pika python-dateutil
-sudo -H python2.7 -m pip install --upgrade tendo paho-mqtt
-sudo -H python3.4 -m pip install --upgrade tendo paho-mqtt
-sudo -H python3.5 -m pip install --upgrade tendo paho-mqtt
-sudo -H python3.6 -m pip install --upgrade tendo paho-mqtt
+sudo -H pip  install --upgrade ephem pytz pika python-dateutil
+sudo -H pip3 install --upgrade ephem pytz pika python-dateutil
+sudo -H python2.7 -m pip install --upgrade tendo paho-mqtt smbus-cffi
+sudo -H python3.4 -m pip install --upgrade tendo paho-mqtt smbus-cffi
+sudo -H python3.5 -m pip install --upgrade tendo paho-mqtt smbus-cffi
+sudo -H python3.6 -m pip install --upgrade tendo paho-mqtt smbus-cffi
 
 sudo apt-get clean && sudo apt-get autoremove
