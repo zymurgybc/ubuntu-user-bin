72199c33936f3e7180693ccda4c3bb962144d231
Ted H <zymurgy.bc@gmail.com>
2018-01-02 12:22:49 -0800

    Moved more Python packages into the update loop so they're more consistently installed.

--- raspberrypi_setup.sh
+++ raspberrypi_setup.sh
@@ -71,8 +71,6 @@ fi
 
 sudo perl -MCPAN -e "install 'YAML'; install 'CPAN'" #; reload CPAN"
 sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
-sudo -H pip  install --upgrade ephem pytz pika python-dateutil
-sudo -H pip3 install --upgrade ephem pytz pika python-dateutil
 
 verPython=("python2.7" "python3.4" "python3.5" "python3.6" )
 for i in "${verPython[@]}"
@@ -80,7 +78,7 @@ do
     echo "Checking for ${i}..."
     if [ ! -z "`which ${i}`" ]; then
         echo "    Found \"`which ${i}`\""
-        sudo -H sh -c "`which ${i}` -m pip install --upgrade tendo paho-mqtt smbus-cffi"
+        sudo -H sh -c "`which ${i}` -m pip install --upgrade ephem pytz pika python-dateutil tendo paho-mqtt smbus-cffi"
     else
         echo "    ${i} does not appear to be available."
     fi
