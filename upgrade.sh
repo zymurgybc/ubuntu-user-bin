b847d1be01c6394b7bd501ec6e9ebc00dfb91166
Ted H <zymurgy.bc@gmail.com>
2018-06-18 16:50:41 -0700

    removing the flash plugin for chrome

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
    sudo apt-get update          \
 && sudo apt-get -y upgrade      \
 && sudo apt-get -y dist-upgrade \
-&& sudo apt-get -y purge libreoffice wolfram-* sonic-pi scratch 2>/dev/null \
+&& sudo apt-get -y purge libreoffice wolfram-* sonic-pi scratch flashplugin-installer 2>/dev/null \
 && sudo apt-get -y autoremove \
 && sudo apt     -y autoremove
 
