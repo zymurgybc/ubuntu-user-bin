a329daaf3bdb29d1de013d2451929fc883bad737
Ted H <zymurgy.bc@gmail.com>
2018-10-23 07:36:27 -0700

    reviewing script, altered white space

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,7 @@
 && sudo apt-get -y upgrade      \
 && sudo apt-get -y dist-upgrade \
 && sudo apt-get -y purge libreoffice wolfram-* sonic-pi scratch flashplugin-installer 2>/dev/null \
-&& sudo apt-get -y autoremove \
+&& sudo apt-get -y autoremove   \
 && sudo apt     -y autoremove
 
 #echo upgrade | sudo -H cpan
