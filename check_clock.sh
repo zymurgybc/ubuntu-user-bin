e5068a9f630158ae8ee1cb015459d542d86d6ab3
Ted H <zymurgy.bc@gmail.com>
2018-10-29 12:19:21 -0700

    Minor edits from updating the pi-clock

--- check_clock.sh
+++ check_clock.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #http://stackoverflow.com/questions/16828035/linux-command-to-check-if-a-shell-script-is-running-or-not
-result=`sudo ps aux | grep -i "[c]lock\.py" | grep -v "grep" | wc -l`
+result=`sudo ps aux | grep -i "[Cc]lock27\.py" | grep -v "grep" | wc -l`
 if [ $result -lt 1 ]; then
     echo "script is not running"
-    sudo /etc/init.d/clock.py &
+    #sudo /etc/init.d/clock.py &
 #else
 #    echo "script is running"
 fi
