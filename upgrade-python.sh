7261c2d83ebbcfb9ec5d1ddcae692d1db890072f
Ted H <zymurgy.bc@gmail.com>
2018-11-18 12:59:18 -0800

    More tweaks getting python setup more consistently across machines.  Updates are now confined to a specific script called by other scripts.

---
+++ upgrade-python.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+verPython=("python2.7" "python3.4" "python3.5" "python3.5m" "python3.6" "python3.6m" "python3.7" )
+for i in "${verPython[@]}"
+do
+    echo "Checking for ${i}..."
+    if [ -f "/usr/bin/${i}" ]; then
+        echo "    Found \"/usr/bin/${i}\""
+        sudo -H sh -c "/usr/bin/${i} -m pip install --upgrade pip ephem pytz pika python-dateutil tendo paho-mqtt cffi smbus-cffi"
+    else
+        echo "    \"/usr/bin/${i}\" does not appear to be available."
+    fi
+
+    # There may be more in a virtual environment, too
+    if [ ! -z "`which ${i}`" ]; then
+        echo "    Found \"`which ${i}`\""
+        sudo -H sh -c "`which ${i}` -m pip install --upgrade pip ephem pytz pika python-dateutil tendo paho-mqtt cffi smbus-cffi"
+
+# http://ouimeaux.readthedocs.io/en/latest/installation.html
+# wemo wrapper in python :-)
+# python3 -m pip install ouimeaux
+    else
+        echo "    ${i} does not appear to be available."
+    fi
+done
+
