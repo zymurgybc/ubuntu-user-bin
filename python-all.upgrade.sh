362a28f5b6c3cff12ab10e35adb7667ed1ff3c49
zymurgybc <zymurgy.bc@gmail.com>
2021-09-24 06:53:58 -0700

    minor updates, a couple new images

--- python-all.upgrade.sh
+++ python-all.upgrade.sh
@@ -7,3 +7,9 @@ for py in `ls /usr/bin/python?.?`; do
   sudo -H $py -m pip install --upgrade freeze pip
   $py -m pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | sudo -H xargs $py -m pip install --upgrade
 done
+
+venv = (/usr /opt/AlexaPi/src/env2.7 /opt/AlexaPi/src/env3.5)
+for py in venv; do
+  sudo -H $py -m pip install --upgrade freeze pip
+  $py -m pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | sudo -H xargs $py -m pip install --upgrade
+done
