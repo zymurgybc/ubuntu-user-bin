12964407376e62864929bb05a85335daf8bf2c5d
Ted H <zymurgy.bc@gmail.com>
2022-01-29 13:57:12 -0800

    Added CUDA driver status/upgrade for one computer

---
+++ upgrade.conductor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CUDA_BIN=/usr/local/cuda-11.5/bin/
+
+echo "==========="
+echo "nvidia-smi"
+nvidia-smi
+echo
+echo "==========="
+echo "nvcc -V  # NVidia Cuda Compiler"
+echo "==========="
+${CUDA_BIN}/nvcc -V
+echo "==========="
+
+CUDA_INSTALLLER=/home/theather/Downdloads/NVidia/cuda_11.5.0_495.29.05_linux.run
+if [ -x "${CUDA_INSTALLER}" ]; then
+    . "${CUDA_INSTALLER}"
+fi
+
