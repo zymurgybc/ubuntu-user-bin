e9266399216afbec52e25550a87096d5e8520a8a
Ted H <zymurgy.bc@gmail.com>
2022-01-29 11:36:48 -0800

    Fixing some small problems appear brought on by a major release upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -5,11 +5,20 @@
 && sudo apt-get -y autoremove   \
 && sudo apt     -y autoremove
 
+mkdir -p "${HOME}/tmp"
+
 if [ -f "${HOME}/bin/upgrade-python.sh" ]; then
-    if [ -f "${HOME}/upgrade-python.log" ]; then
-        rm "${HOME}/upgrade-python.log"
+    if [ -f "${HOME}/tmp/upgrade-python.log" ]; then
+        rm "${HOME}/tmp/upgrade-python.log"
     fi
-    ${HOME}/bin/upgrade-python.sh | tee -a ${HOME}/upgrade-python.log
+    ${HOME}/bin/upgrade-python.sh | tee -a ${HOME}/tmp/upgrade-python.log
+fi
+
+if [ -f "${HOME}/bin/upgrade.${HOSTNAME}.sh" ] then
+    if [ -f "${HOME}/tmp/upgrade.${HOSTNAME}.log" ]; then
+        rm "${HOME}/tmp/upgrade.${HOSTNAME}.log"
+    fi
+    . "${HOME}/bin/upgrade.${HOSTNAME}.sh" 2>&1 | tee -a "${HOME}/tmp/upgrade.${HOSTNAME}.log"
 fi
 
 #echo upgrade | sudo -H cpan
