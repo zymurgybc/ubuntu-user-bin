94128f6cfc430c4893280e170f129aa3935a646a
Ted H <zymurgy.bc@gmail.com>
2022-11-27 09:52:50 -0800

    2022-11-27 changes

---
+++ Launch-BingRewards.NETCore.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+TERM=vt100
+#/usr/bin/dotnet "/home/theather/source/github.com/zymurgybc/BingRewards.NetCore/BingRewards.NETCore/bin/Release/net6.0/BingRewards.NETCore.dll"
+/usr/bin/dotnet "/home/theather/source/github.com/zymurgybc/Core/BingRewards.NetCore/BingRewards.NETCore/bin/Release/net6.0/BingRewards.NETCore.dll"
