41ea9394916e1114947c50dd36515142bcb90d2c
Ted H <zymurgy.bc@gmail.com>
2023-07-22 03:18:50 -0700

    Working to fix whatismyip.com command and failed to improve it as it requires a JS browser

--- raspberrypi_setup.sh
+++ raspberrypi_setup.sh
@@ -85,7 +85,7 @@ fi
 sudo perl -MCPAN -e "install 'CPAN::DistnameInfo'; install 'YAML'; install 'CPAN'" #; reload CPAN"
 sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
 @ Updates to run dhsupdate.pl
-sudo perl -MCPAN -e "install 'LWP:UserAgent'; "
+sudo perl -MCPAN -e "install 'LWP::Protocol::https'; install 'LWP::Simple'; 'LWP:UserAgent'; "
 
 if [ -f "${HOME}/bin/upgrade-python.sh" ]; then
     ${HOME}/bin/upgrade-python.sh
