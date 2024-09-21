a27bd343971c96dd7bc0688bad1b7b6c4661c246
Ted H <zymurgy.bc@gmail.com>
2024-09-21 10:38:48 -0700

    Fixed a couple user scripts

--- My_Status_mqtt.sh
+++ My_Status_mqtt.sh
@@ -3,7 +3,7 @@ USER_HOME=/home/theather
 VENV=${USER_HOME}/.local/python3
 
 cd ${USER_HOME}/bin
-PATH=${VENV}/bin/:${USER_HOME}/bin/:%PATH%
+PATH=${VENV}/bin/:${USER_HOME}/bin/:${PATH}
 PYTHON_PATH=${VENV}/bin:${USER_HOME}/bin:${PYTHON_PATH}
 
 #${USER_HOME}/.local/python3/bin/python3.11 -m My_Status_mqtt 2>&1 | /usr/bin/ts '[\%Y-\%m-\%d \%H:\%M:\%S]' >> /var/log/mqtt_client.log
