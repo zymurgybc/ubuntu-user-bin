767aaae3e6da316e832f84fd61c09070e8265984
Ted H <zymurgy.bc@gmail.com>
2025-01-05 13:50:16 -0800

    script debugging and logging

--- My_Status_mqtt.sh
+++ My_Status_mqtt.sh
@@ -1,6 +1,7 @@
 
 USER_HOME=/home/theather
 VENV=${USER_HOME}/.local/python3
+MQTT_LOG=/var/log/mqtt_client.log
 
 if [ -x "${VENV}/bin/python3.12" ]; then
    PYTHON_EXEC="${VENV}/bin/python3.12"
@@ -12,5 +13,10 @@ cd ${USER_HOME}/bin
 #PATH=${VENV}/bin/:${USER_HOME}/bin/:${PATH}   # Handled by activate script?
 PYTHON_PATH=${VENV}/bin:${USER_HOME}/bin:${PYTHON_PATH}
 
+echo MQTT_LOG="${MQTT_LOG}"       | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${MQTT_LOG}
+echo USER_HOME="${USER_HOME}"     | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${MQTT_LOG}
+echo VENV="${VENV}"               | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${MQTT_LOG}
+echo PYTHON_EXEC="${PYTHON_EXEC}" | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${MQTT_LOG}
+
 . ${VENV}/bin/activate
-${PYTHON_EXEC} -m My_Status_mqtt 2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> /var/log/mqtt_client.log
+${PYTHON_EXEC} -m My_Status_mqtt 2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${MQTT_LOG}
