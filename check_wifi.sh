d9ebad116d3420502ea0afe43d3c0e85ed5d9e4e
Ted H. <zymurgy.bc@gmail.com>
2025-01-14 07:26:45 -0800

    normallise the timestamp output for readability

--- check_wifi.sh
+++ check_wifi.sh
@@ -2,12 +2,12 @@
 # Apr 2024
 
 TEST_LOG=/var/log/check_network.log
-ping -c4 192.168.73.1 | ts >> ${TEST_LOG}
+ping -c4 192.168.73.1 | ts '[\%Y-\%m-\%d \%H:\%M:\%S]' >> ${TEST_LOG}
 
 if [ $? != 0 ]
 then
-  echo ========== Restarting Network  | ts >> ${TEST_LOG}
-  sudo ifconfig wlan0 down | ts >> ${TEST_LOG}
+  echo ========== Restarting Network  | ts '[\%Y-\%m-\%d \%H:\%M:\%S]' >> ${TEST_LOG}
+  sudo ifconfig wlan0 down | ts '[\%Y-\%m-\%d \%H:\%M:\%S]' >> ${TEST_LOG}
   sleep 5
-  sudo ifconfig wlan0 up   | ts >> ${TEST_LOG}
+  sudo ifconfig wlan0 up   | ts '[\%Y-\%m-\%d \%H:\%M:\%S]' >> ${TEST_LOG}
 fi
