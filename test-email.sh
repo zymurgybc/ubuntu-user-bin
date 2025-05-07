22332964824dda90730894e875fd51f72f72a214
Ted Heatherington <theather@raspberrypi>
2025-05-07 12:25:41 -0700

    reformatted the message to add a bit more context

--- test-email.sh
+++ test-email.sh
@@ -1,2 +1,6 @@
-echo "Dummy message from ${HOSTNAME}." | mail --debug-level=9 --config-verbose -s "Simple Test Email" zymurgy.bc@gmail.com
+#!/usr/bin/env bash
+
+#echo "Dummy message from ${HOSTNAME}." | mail --debug-level=9 --config-verbose -s "Simple Test Email" -- ${USER}
+echo "Dummy message from ${HOSTNAME}.
+$(ls -al `which sendmail`)" | mail -s "Simple Test Email" -- ${USER}
 echo $?
